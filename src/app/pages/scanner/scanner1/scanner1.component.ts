41eaee920f9077283666e9ab51aa0038815bba62
lukslu5 <luca-stinn@web.de>
2023-06-15 14:45:44 +0200

    Scan works

--- src/app/pages/scanner/scanner1/scanner1.component.ts
+++ src/app/pages/scanner/scanner1/scanner1.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface Product {
+export interface Product {
   error: string;
   asin: string;
   name: string;
@@ -23,60 +24,66 @@ interface Product {
   validated: string;
 }
 
-
 @Component({
   selector: 'app-scanner1',
   templateUrl: './scanner1.component.html',
   styleUrls: ['./scanner1.component.scss'],
-  
 })
-
-export class Scanner1Component {
-  constructor(private http: HttpClient) { }
-
-
+export class Scanner1Component implements OnInit {
   savedResult: string = "0000000000000";
+  products: Product[];
 
-  scanSuccessHandler(result: string){
-    if(this.savedResult != result)
-    {
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.makeApiRequest('4011094102033');
+  }
+
+  scanSuccessHandler(result: string) {
+    if (this.savedResult !== result) {
       this.savedResult = result;
-    
-      this.makeApiRequest(this.savedResult)
+      this.makeApiRequest(this.savedResult);
       console.log(this.savedResult);
     }
   }
 
   makeApiRequest(barcode: string): void {
-    const url = `http://localhost:3000/api?ean=4011094102033`;
-    //const url = `http://localhost:3000/api?ean=${barcode}`;
-  
+    const url = `http://localhost:3000/api?ean=${barcode}`;
+
     this.http.get(url, { responseType: 'text' }).subscribe(
       (data: string) => {
         // Parse the plain text response manually
-        const products: Product[] = this.parseResponse(data);
-        console.log(products);
+        this.products = this.parseResponse(data);
+        console.log(this.products);
+
+        // Pass the products array to Scanner2Component
+        this.router.navigate(['/scanner/info'], { queryParams: { products: JSON.stringify(this.products) } });
+
       },
       (error) => {
         console.error(error);
       }
     );
   }
+
   private parseResponse(data: string): Product[] {
     const products: Product[] = [];
     const lines = data.split('---');
-  
+
     for (const line of lines) {
       const entries = line.split('\n');
       const product: Partial<Product> = {};
-  
+
       let isErrorObject = false;
-  
+
       for (const entry of entries) {
         const [key, value] = entry.split('=');
         const trimmedKey = key ? key.trim().toLowerCase() : '';
         const trimmedValue = value ? value.trim() : '';
-  
+
         if (trimmedKey && trimmedValue) {
           if (trimmedKey === 'error') {
             isErrorObject = true;
@@ -85,14 +92,12 @@ export class Scanner1Component {
           product[trimmedKey as keyof Product] = trimmedValue;
         }
       }
-  
+
       if (Object.keys(product).length > 0 && !isErrorObject) {
         products.push(product as Product);
       }
     }
-  
+
     return products;
   }
-  
-  
 }
--- src/app/pages/scanner/scanner2/scanner2.component.ts
+++ src/app/pages/scanner/scanner2/scanner2.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../scanner1/scanner1.component';
 
 @Component({
   selector: 'app-scanner2',
   templateUrl: './scanner2.component.html',
-  styleUrls: ['./scanner2.component.scss']
+  styleUrls: ['./scanner2.component.scss'],
 })
-export class Scanner2Component {
+export class Scanner2Component implements OnInit {
+  products: Product[];
 
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      if (params.products) {
+        this.products = JSON.parse(params.products);
+      } else {
+        // Handle case when products are not available
+      }
+    });
+  }
 }
